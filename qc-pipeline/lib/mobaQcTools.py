c8febe4b0b1a7a0d7dd814ae9c0f5487ab6c0e12
Gutorm <gthh@fhi.no>
2019-12-09 10:44:49 +0100

    Better .rst/.yaml logging ... and cleanup
    mqc.py had some old (and duplicated) log methods.
    rules.yaml has some new entries and a little documentation

--- qc-pipeline/lib/mobaQcTools.py
+++ qc-pipeline/lib/mobaQcTools.py
@@ -12,6 +12,7 @@ import re
 import datetime
 from pathlib import Path
 import subprocess
+from datetime import datetime
 
 def plotHist(dataFile, resultFile, column="name of the column", title="no legend??", separator='\s+',
              treshold=0, logx=False):
@@ -47,7 +48,9 @@ def saveYamlResults(files, yamlStruct):
     "files" is the name of the structure without samples/markers (xitems). 
     A .rst file will be produced in addition to the .yaml file (used by snakemake --report)
     Side effect: Deletes yamlStruct["xitems"]
+                 Sets Timestamp in yamlStruct
     """
+    yamlStruct["Timestamp"] = datetime.utcnow().strftime("%Y%m%d %H:%M")+"(UTC)"
     files = Path(files)
     fullFile = files.with_suffix(".yaml.details")
     with open(fullFile, 'w') as file:
@@ -59,11 +62,214 @@ def saveYamlResults(files, yamlStruct):
         yaml.dump(yamlStruct, file)
     # A .rst version used for captions
     rstFile = files.with_suffix(".rst")
+    percentDropped = yamlStruct["actionTakenCount"] / yamlStruct["in"]
     with open(rstFile, 'w') as file:
-        file.write(f'Rule {yamlStruct["Rule order"]}\n\n')
+        file.write(f'Rule {yamlStruct["Rule order"]} ({yamlStruct["rule type"]})\n\n')
         file.write(f'- {yamlStruct["in"]} in\n')
-        file.write(f'- {yamlStruct["actionTakenCount"]} actions on {yamlStruct["rule type"]} \n') # 
-        file.write(f'- {yamlStruct["out"]} left\n')
+        file.write(f'- {yamlStruct["actionTakenCount"]} ({percentDropped:.1%}) {yamlStruct["rule action"]}d\n') # 
+        file.write(f'- {yamlStruct["out"]} left\n\n')
+        file.write(f'{yamlStruct["Timestamp"]}\n')
+
+    
+def plinkBase(path):
+    """ part of the file without the last extention (such as .fam .bed .bim)
+
+    plink often works on a trunk and creates extra files: a becomes a.fam, a.bed, a.bid
+    This function reates this trunk, so /foo/bar/gazonk.fam becomes /foo/bar/gazonk
+    The resulting trunk is typically used as input to plink
+    Only the last '.' is removed so /foo.foo/bar/gazonk.x.fam will become /foo.foo/bar/gazonk.x
+    """
+    return  re.sub(r"\.\w*$","",path)
+
+
+def lt(a,b):
+    return a < b
+def eq(a,b):
+    return a == b
+def gt(a,b):
+    return a > b
+def unknownComp(a,b):
+    print ("Unknown compare operation")
+    return 
+
+
+
+# used to map symbols to actual comparision functions defined previously
+compOperands = {
+    "<": lt,
+    "=": eq,
+    ">": gt
+}
+
+
+def extractSampleList(innFile, sampleFile, subsetFile="/dev/null", 
+                      colName="none", sep='\t', condition="<", treshold=0, cols={1}, ):
+    """ A typical preprocessor to utilities like plink.
+
+    Efficient in the sense that it 
+    is not reading the (huge) files into memory.
+    Takes a csv file innFile (first line with headers, seperator is paramneter "sep") and produces 
+    * a sample list on sampleFile (one columun, for now hardcoded to column 0)
+    * a subset file with at set of numbered columns (cols) that also contains colName (see below)
+    
+    Only columns were the column colName matches the condition of treshhold will be written
+    Returns (number of sample extracted, total samples)
+    Restrictions: 
+    * Assuming the first column in innFile is samplenumber - Only the first column will be written to sampleFile
+    * Assumes only one match for the regExp colName
+    * Columns are ordered in the same way as the innFile : {0,1} is identical to {1,0,0,1}
+    * Outputfiles use the same separator as used for innFile
+    """ 
+    with open(innFile) as fp:
+        
+        # Identifying header column 
+        line = fp.readline()
+        allcols = line.split(sep)
+        regex = re.compile(colName)
+        indx = [i for i, item in enumerate(allcols) if re.search(regex, item)]
+        # print(f"{colName} is column {indx}")
+        matches = 0
+        compare = compOperands.get(condition, unknownComp)
+        if compare == unknownComp :
+            print (f"Cannot compare using: '{condition}'")
+            return
+        # These lines are data, indx[0] is the index  of the column we found interesting
+        sample = open(sampleFile,"w+")
+        subset = open(subsetFile,"w+")
+        for line in enumerate(fp):
+            allcols = line[1].split(sep)
+            if compare(float(allcols[indx[0]]) , treshold): 
+                # File with only sample number
+                sample.write(allcols[0] + "\n")
+                # File with more columns, and always include the treshold column
+                # subsetc is a relevant subset of the columns 
+                subsetc = [allcols[index] for index in cols]                
+                subset.write(sep.join(map(str,subsetc)) + sep + allcols[indx[0]] + "\n" )
+                matches += 1
+    totalLines = line[0] + 1 # enumerates from 0, and we read a line manually
+    sample.close()
+    subset.close()
+    return (matches,totalLines)
+
+    
+
+    
+def dict_count_items(fil, cols=[0,1], warn=True):
+    """
+    Creates a dictionary with as key concatenation of the strings of the columns found in fil.
+    Values are the number of times that key is found
+    Returns the dictionary and the largest key value. In many scenarios, 1 is wanted here,
+    meaning no duplicates. 
+    if warn=True, will warn about key value > 1
+    fil is expected to be a csv file with whitespace as delimiters
+    First Column in the file is number 0 (standar Python)
+    (Hint for when this is used to compare the columns of two files: You can reduce memory usage by 
+    making a dictionary of the smallest file and searchin/iterating for matches through the largest
+
+    Will print error/warning if dictionary is empty or if replicates found
+    """
+    # pandas is overkill here, but since we will use it anyway ...
+    # Grab the relevant columns only
+    try: 
+        df = pd.read_csv(fil, usecols=cols, delim_whitespace=True, header=None)
+    except Exception as e:
+        print(f"Could not open file {fil}, {str(e)}")
+        return
+    # concat them as strings, handling NA
+    df = pd.Series(df.fillna('NA').values.tolist()).map(lambda x: ' '.join(map(str,x)))
+    # ... and finally make a dictionary. Note that we will here also count identical lines.
+    countDict = dict()
+    maxHits = 0
+    for i in df:  
+        countDict[i] = countDict.get(i,0) + 1
+        if ( countDict[i]) > maxHits : 
+            maxHits = countDict[i]
+            sample = i
+    if len(countDict) == 0 : print("ERROR: 0 sized dictionary after reading ",fil)
+    if maxHits>1 and warn: print(f"WARNING: sample/marker {sample} found {maxHits} times in {fil}. Might not be the only nonunique ...")
+    return countDict, maxHits
+
+def lookupDict(fil, indx=1):
+    """ Creates a lookup dictionary from fil, width column indx as index
+
+    fil is a whitespice separated csv
+    indx is the column that you will lookup on later, 0 is the first column
+    """
+    try: 
+        all = pd.read_csv(fil, delim_whitespace=True, header=None).astype(str)
+    except Exception as e:
+        print(f"Could not open mapping file {fil}, {str(e)}")
+        return
+    indexCol = all[indx]  # get the index column
+    # all = all.drop([indx], axis=1) # drop it - figured out it gave confusing output
+    all = all.apply(" ".join, axis=1) # make each row a single string
+    return dict(zip(indexCol,all))
+
+
+def checkUpdates(preQc, postQc, cols=[0,1], indx=1, sanityCheck="none",
+                 fullList=False,  mapFile="", mapIndx=1):
+    """
+    Return number of updates due to a QC-test as well as a structure suited for a export as a yaml-file
+    The scenario is that a QC method/step had a dataset (file indData) and produced outData.
+    Some items got filtered out/changed
+    pre/postQC are tab-serparated csv-files with the same amount of columns
+    The optional sanityCheck parameter will give error message as follows, depending to its value
+       removal: Number of elements taken action on (removed) = size of preQc-postQc
+       updated: size of preQc = postQc  
+       anything different from the above): No tests performed
+    Only columns passed by cols are used to compare input/and output.
+    indx is only necessary if fullList is True. indx is used to genereate a list (usually of samples/markers)
+    from column nr indx
+    indx is sample-id/marker-id. indx=0 is the first column, default is second column. 
+    mapFile is relevant when fullList=True and is the file that mapped values from preQc to postQc 
+    and is used to show what the missing/renamed elements were named before
+    mapIndx indicates the column of the mapFile that corresponds to indx in the datafile
+    
+    The yaml-structure will always contain the number of input samples/markers as well as sample/markers removed/remaining.
+    
+    """
+    # dictionaly with only relevant columns
+    (outDict,m) = dict_count_items(postQc, cols)
+    result = {
+        "in":   0,        # will count lines from the 'in' file
+        "out": len(outDict), 
+        "xitems": [],    # poplulated later by samples/markers if fullList is True
+        "actionTakenCount": 0 # Not found in dictionary, so it is the effect of the qc-rule on the inputfile
+        }
+    haveDict = False
+    if mapFile != "":     # Setting up a dictionary 'lookup' to 
+                          # lookup the original value if postQc has changed
+        lookup = lookupDict(mapFile, mapIndx)
+        haveDict = True
+        result["mapFile"] = mapFile
+        #import json
+        #print ("Saving lookupdict to lookup.json")
+        #json.dump(lookup, open("lookup.json", 'w' ) )
+        
+    for line in open(preQc):
+        result["in"] += 1 
+        allcols = line.split()
+        subsetc = [allcols[index] for index in cols]
+        # concatenating so we can look up the strings with corresponding field from postQc file
+        key = " ".join(map(str,subsetc))
+        if (outDict.get(key,0) == 0):
+            result["actionTakenCount"] += 1 
+            item = str(allcols[indx])    # being the sample or the marker
+            if fullList:
+                changed = f"{item} - had {key}"  # This item was not found in preQc
+                if haveDict:
+                    mappingRule = lookup.get(item, 'ooops: missing')
+                    changed = f'{changed} changed by mapping {mappingRule}'
+                result["xitems"].append(changed)    # sample/marker id(s)
+                
+    if sanityCheck == 'updated':  # for updates, we dont't want to loose samples
+        if (result["out"]) != result["in"] : 
+            print(f"Warning: {preQc} -> {postQc}: Update expected, but number of unique items have changed "
+                  f"from {result['in']} to {result['out']}" ) 
+    elif sanityCheck == 'removal':       # for removals, we want out + removed = in
+        if (result["actionTakenCount"] + result["out"]) != result["in"] : 
+            print(f"Warning: {preQc} -> {postQc}: remaining + removed samples != original number")
+    return result
     
 def plinkBase(path):
     """ part of the file without the last extention (such as .fam .bed .bim)
@@ -271,26 +477,7 @@ def log(logfile, message="Nothing logged", mode="a"):
     Placeholder until we figure out how logging really needs to be done. Default appends message to logfile
     """
     with open(logfile, mode) as myfile:
-        myfile.write('{:%Y-%m-%d %H:%M:%S} '.format(datetime.datetime.now()) + message)
-    return
-
-def resultLog(logfile, message="Nothing logged", mode="w+" ):
-    """
-    Placeholder until we figure out how results will be reporte. Default creates a new message to logfile
-    No formating is done, in contrary to log()
-    Currently it logs to logfile, but also to a global file (always in append-mode) 
-    where it also shows the path of the result.
-    The (crude) idea of the global file is to make it easier to make a comple (and prettified) report. 
-    The name should be a variable somehow
-    Have not tested this with threads, it will probably fail unless you reset the file and use append mode
-    """
-    with open(logfile, mode) as myfile:
-        myfile.write(message)
-
-    # Dirty - hardcoded name here ...
-    mergedResults = Path(logfile).parent/"mergedResults.txt"
-    with open(mergedResults,"a") as myfile:
-        myfile.write( f"(ref {logfile} ) " + message)
+        myfile.write(f'{datetime.utcnow().strftime("%Y%m%d %H:%M")} {message}')
     return
 
 def _make_gen(reader):
