5ab26e82381636f81aa8fd7a685f4f2bc855ee05
Gutorm <gthh@fhi.no>
2020-09-09 10:34:30 +0200

    now compares files and not directories
    Stem is the imput. We don't need identical names anymore

--- qc-pipeline/lib/plinkDiff.sh
+++ qc-pipeline/lib/plinkDiff.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-# Used to compare results of two different QC-runs Checks two
-# directories, comparing all plink .fam and .bim files. For bim files,
-# se second column (variant identifier) is ignored (id)
+# Used to compare results of two different QC-runs. Compares two
+# bedsets, comparing .fam and .bim files. For bim files,
+# the second column (variant identifier) is ignored (id)
 #
 # Will sort files before doing a cmp
-# The two parameters are the name of the directories
+# The two parameters are the name of the two trunks
 
 if [ ! $# -eq 2 ]
 then
-    echo Usage: $0 dir1 dir2
-    echo Compares all .fam and .bim files found in dir1 and dir2
-    echo File names must match \(sorry about that\) and dir1 is master-dir
+    echo Usage: $0 bedsetTrunk1 bedsetTrunk2
+    echo Compares  bedsetTrunk1 .fam and .bim files with corresponding bedsetTrunk2
     echo Creates a subdirectory in current directory called sort with
     echo sorted \(and for bim files without column 2\) files
     echo 
@@ -19,21 +18,15 @@ fi
 
 # set -x  # For debug/echo
 mkdir -p sort
-for file in $1/*.{bim,fam};
-do
-    #echo "$file";
-    file1=$1/$(basename -- "$file");
-    sortedFile1=sort/$(basename -- "$file");
-    file2=$2/$(basename -- "$file");
-    echo comparing $file1 and $file2 with comm -3
-    extension="${file2##*.}"
-    if [ $extension == "fam" ]
-    then
-        sort $file1 > $sortedFile1
-        sort $file2 | comm -3 $sortedFile1 -
-    else #bim file: Ignore markername and sort strands
-        awk '{if ($5<$6) {print $1,$3,$4,$5,$6} else {print $1,$3,$4,$6,$5}}' $file1 | sort > $sortedFile1
-        awk '{if ($5<$6) {print $1,$3,$4,$5,$6} else {print $1,$3,$4,$6,$5}}' $file2 | sort | comm -3 $sortedFile1 -
-    fi
+file1=$1;
+sortedFile1=sort/$(basename -- "$file1");
+file2=$2;
+echo comparing $file1.fam  and $file2.fam files with comm -3
+# fam files are pure sort
+sort $file1.fam > $sortedFile1.fam
+sort $file2.fam | comm -3 $sortedFile1.fam -
+echo comparing $file1.bim  and $file2.bim files with comm -3
+#bim files: Ignore markername and sort order strands ($5 and $6)
+awk '{if ($5<$6) {print $1,$3,$4,$5,$6} else {print $1,$3,$4,$6,$5}}' $file1.bim | sort > $sortedFile1.bim
+awk '{if ($5<$6) {print $1,$3,$4,$5,$6} else {print $1,$3,$4,$6,$5}}' $file2.bim | sort | comm -3 $sortedFile1.bim -
     
-done
