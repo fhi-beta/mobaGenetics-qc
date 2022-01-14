f89740f3df352f07f355540fdcdcbf8ff39cac09
Gutorm Høgåsen <gthh@moba-store-home.novalocal>
2022-01-14 15:43:29 +0100

    moved to elsewhere

--- methylation/cleanUp.sh
+++
@@ -1,8 +0,0 @@
--
--        if [ "$rule" == "third_part" ]
--        then
--
--                echo snakemake --core $cores --configfile input/globalConfig.yaml tmp_config/$set --snakefile Snakefile clean_up
--                     snakemake --core $cores --configfile input/globalConfig.yaml tmp_config/$set --snakefile Snakefile clean_up
--        fi	
-	
