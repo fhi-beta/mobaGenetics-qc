9dae3728a134d376e39d3d6a0e2b8359e6429b1f
mvaudel <mvaudel@gmail.com>
2022-12-04 20:37:03 +0100

    Clean-up

--- qc-pipeline/lib/mobaQcTools.py
+++ qc-pipeline/lib/mobaQcTools.py
@@ -501,7 +501,7 @@ def missing_genotype_rate(
         sample = True,
         threshold=0.1,
         result_file = '/dev/null',
-        plot_file = False
+        plot_file = False,
         plinklocal
 ):
     """Runs plink --geno or --mind and produces output
