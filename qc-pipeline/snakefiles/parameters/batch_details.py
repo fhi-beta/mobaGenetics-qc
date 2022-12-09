0b0bc7a96d9566e317a40d48ea07ddfd49f1d30e
mvaudel <mvaudel@gmail.com>
2022-12-09 10:32:00 +0100

    Snp table for Rotterdam 1

--- qc-pipeline/snakefiles/parameters/batch_details.py
+++ qc-pipeline/snakefiles/parameters/batch_details.py
@@ -9,6 +9,7 @@ batch_mobagenetics_10_name = {
     "snp014": "Rotterdam2"
 }
 batch_snp_table = {
+    "snp012": "/mnt/archive2/MomicsSource/snpArray/snp012/ROTTERDAM1/rotterdam1-aux/qc/snp_table_rotterdam1.txt",
     "snp014": "/mnt/archive2/MomicsSource/snpArray/snp014/ROTTERDAM2/delivery-fhi/data/raw-data/snptable/snp_table_rotterdam2.txt"
 }
 
