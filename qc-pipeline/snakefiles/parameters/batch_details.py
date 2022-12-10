ce4100c187eaa9d4d9f5489eb3df4040a4ddcc5c
mvaudel <mvaudel@gmail.com>
2022-12-10 20:32:01 +0100

    More documentation on the batches.

--- qc-pipeline/snakefiles/parameters/batch_details.py
+++ qc-pipeline/snakefiles/parameters/batch_details.py
@@ -4,15 +4,31 @@ batch_genotyping_chip = {
     "snp001": "Illumina Human Core Exome 12 v.1.1",
     "snp002": "Illumina Human Core Exome 12 v.1.1",
     "snp003": "Illumina Human Core Exome 24 v.1.0",
+    "snp007": "Illumina Human Omni Express 24 v.1.0",
+    "snp008": "Illumina Human Omni Express 24 v.1.0",
+    "snp009": "Illumina Infinium Omni Express 24 v.1.2",
+    "snp011": "Illumina Infinium Omni Express 24 v.1.2",
     "snp012": "Illumina Global Screening Array MD v1.0",
-    "snp014": "Illumina Global Screening Array MD v1.0"
+    "snp014": "Illumina Global Screening Array MD v1.0",
+    "snp015a": "Illumina Global Screening Array MD v1.0",
+    "snp015b": "Illumina Global Screening Array MD v3.0",
+    "snp016a": "Illumina Global Screening Array MD v3.0",
+    "snp016b": "Illumina Global Screening Array MD v3.0"
 }
-batch_mobagenetics_10_name = {
+batch_name = {
     "snp001": "Harvest m12b",
     "snp002": "Harvest m12a",
     "snp003": "Harvest m24",
+    "snp007": "Norment Jan15",
+    "snp008": "Norment Jun15",
+    "snp009": "Norment May16",
+    "snp011": "Ted1",
     "snp012": "Rotterdam1",
-    "snp014": "Rotterdam2"
+    "snp014": "Rotterdam2",
+    "snp015a": "Norment Feb20 1",
+    "snp015b": "Norment Feb20 3",
+    "snp016a": "Norment Aug20 996",
+    "snp016b": "Norment Aug20 1029"
 }
 batch_snp_table = {
     "snp012": "/mnt/archive2/MomicsSource/snpArray/snp012/ROTTERDAM1/rotterdam1-aux/qc/snp_table_rotterdam1.txt",
