63d156c891e6b7763b29cd2c582b2975faffaa4a
mvaudel <mvaudel@gmail.com>
2023-02-03 10:56:11 +0100

    Debug

--- qc-pipeline/snakefiles/parameters/variables.py
+++ qc-pipeline/snakefiles/parameters/variables.py
@@ -34,6 +34,8 @@ github_docs = Path(os.path.dirname(workflow.basedir)) / 'docs'
 tmpMod1 = base/'mod1-data-preparation/'
 tmpMod2 = base/'mod2-data-preparation/'
 tmpMod3 = base/'mod3-good-markers/'
+# tmpMod2 = base/'mod2-genetic-relationship/'
+# tmpMod3 = base/'mod3-population-clustering/'
 tmpMod4 = base/'mod4-samples-unrelated/'
 tmpMod5 = base/'mod5-shaping-preparation/'
 resultPath = base/'results/'
@@ -41,7 +43,7 @@ resultPath = base/'results/'
 ### Batch settings
 # batches = ['snp001', 'snp002', 'snp003', 'snp007', 'snp008', 'snp009', 'snp010', 'snp011', 'snp012', 'snp014', 'snp015a', 'snp015b', 'snp016a', 'snp016b', 'snp017a', 'snp017b', 'snp017c', 'snp017d', 'snp017e', 'snp017f', 'snp018a', 'snp018b', 'snp018c', 'snp018d', 'snp018e']
 batches = ['snp012', 'snp014']
-# batches = ['snp014']
+batches = ['snp014']
 
 
 ### workflow settings ##################################
