1c1a74bf8d8070c5f7c2311d594b2a358b2e9d88
mvaudel <mvaudel@gmail.com>
2023-06-16 14:54:57 +0200

    clean-up

--- qc-pipeline/snakefiles/parameters/variables.py
+++ qc-pipeline/snakefiles/parameters/variables.py
@@ -44,7 +44,6 @@ resultPath = base/'results/'
 ### Batch settings
 # batches = ['snp001', 'snp002', 'snp003', 'snp007', 'snp008', 'snp009', 'snp010', 'snp011', 'snp012', 'snp014', 'snp015a', 'snp015b', 'snp016a', 'snp016b', 'snp017a', 'snp017b', 'snp017c', 'snp017d', 'snp017e', 'snp017f', 'snp018a', 'snp018b', 'snp018c', 'snp018d', 'snp018e']
 batches = ['snp012', 'snp014']
-batches = ['snp014']
 
 
 ### workflow settings ##################################
