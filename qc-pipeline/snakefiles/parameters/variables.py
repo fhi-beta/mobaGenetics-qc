aec5137a12b1d4e20a69bf36de0f9775a3ffc406
Øystein Kapperud <Oystein.Kapperud@fhi.no>
2024-04-21 15:06:34 +0200

    Basic implementation of module 5: pre-phasing (needs logging and debugging)

--- qc-pipeline/snakefiles/parameters/variables.py
+++ qc-pipeline/snakefiles/parameters/variables.py
@@ -23,12 +23,14 @@ libdir = os.path.abspath(os.path.join(os.path.dirname(workflow.basedir), 'lib'))
 bcftools = os.path.abspath(os.path.join(os.path.dirname(workflow.basedir), 'bin/bcftools-1.7/bcftools'))
 vcftools = os.path.abspath(os.path.join(os.path.dirname(workflow.basedir), 'bin/vcftools-0.1.13/vcftools'))
 plinklocal = Path(os.path.dirname(workflow.basedir))/'bin'/'plink-1.90b5.4'/'plink'
+hrc1000g = Path(os.path.dirname(workflow.basedir))/'bin'/'HRC-100G'/'HRC-1000G-check-bim.pl'
 plink2local = Path(os.path.dirname(workflow.basedir))/'bin'/'plink-1.90b5.4'/'plink'
 flashpca = Path(os.path.dirname(workflow.basedir))/'bin'/'flashpca_x86-64'
 kinglocal = Path(os.path.dirname(workflow.basedir))/'bin'/'Linux-king'/'king'
 
 # Resources
 high_ld_regions_hg19 = Path(os.path.dirname(workflow.basedir))/'resources'/'high-ld-regions-hg19'
+hrc_sites = Path(config['hrc_sites'])
 
 # Folder paths
 tmp_path = Path(config['output_base']) / 'tmp'
@@ -39,14 +41,15 @@ tmpMod1 = base/'mod1-data-preparation'
 tmpMod2 = base/'mod2-genetic-relationship'
 tmpMod3 = base/'mod3-population-clustering'
 tmpMod4 = base/'mod4-good_markers'
-tmpMod5 = base/'mod5-samples_unrelated'
-tmpMod6 = base/'mod6-phasing-preparation'
+tmpMod5 = base/'mod5-pre-phasing'
+# tmpMod5 = base/'mod5-samples_unrelated'
+# tmpMod6 = base/'mod6-phasing-preparation'
 resultPath = base/'results'
 
 ### Batch settings
 batches = ['snp001', 'snp002', 'snp003', 'snp007', 'snp008', 'snp009', 'snp010', 'snp011', 'snp012', 'snp014', 'snp015a', 'snp015b', 'snp016a', 'snp016b', 'snp017a', 'snp017b', 'snp017c', 'snp017d', 'snp017e', 'snp017f', 'snp018a', 'snp018b', 'snp018c', 'snp018d', 'snp018e']
 batches = ['snp007', 'snp008', 'snp009', 'snp011', 'snp012', 'snp014', 'snp015a', 'snp015b', 'snp016a', 'snp016b', 'snp017a', 'snp017b', 'snp017c', 'snp017d', 'snp017e', 'snp017f', 'snp018a', 'snp018d'] # Checked up to module 3
-batches = ['snp012'] # Testing module 4
+# batches = ['snp012'] # Testing module 4
 
 
 ### workflow settings ##################################
