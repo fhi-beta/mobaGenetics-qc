957a61d20ca6be27426cddbdbd94bca830d07a73
Øystein Kapperud <Oystein.Kapperud@fhi.no>
2024-05-29 14:39:43 +0200

    report test

--- qc-pipeline/snakefiles/utils/pre_imputation_report.py
+++ qc-pipeline/snakefiles/utils/pre_imputation_report.py
@@ -1,7 +1,7 @@
 import pandas as pd
 def write_report(output_filename, batch, bedset):
     md_file = open(output_filename, "a")
-    md_file.write(f"#Pre-imputation report for batch {batch}")
+    md_file.write(f"# Pre-imputation report for batch {batch}")
     fam_df = pd.read_csv(bedset[2], delim_whitespace=True, header=None,
     names=['FID', 'IID', 'PID', 'MID', 'Sex', 'Phenotype'])
     included_number_of_samples = fam_df.shape[0]
