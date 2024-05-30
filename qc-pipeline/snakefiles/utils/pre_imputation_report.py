95d767d205b717215a498b70cc8f4e34218dca21
Øystein Kapperud <Oystein.Kapperud@fhi.no>
2024-05-30 13:12:07 +0200

    testing writing sample call rate plot to md

--- qc-pipeline/snakefiles/utils/pre_imputation_report.py
+++ qc-pipeline/snakefiles/utils/pre_imputation_report.py
@@ -1,5 +1,7 @@
 import pandas as pd
-def write_report(output_filename, batch, bedset):
+import matplotlib.pyplot as plt
+import mobaQcTools as mqc
+def write_report(output_filename, batch, bedset, imiss, lmiss):
     md_file = open(output_filename, "a")
     md_file.write(f"# Pre-imputation report for batch {batch}")
     fam_df = pd.read_csv(bedset[2], delim_whitespace=True, header=None, names=['FID', 'IID', 'PID', 'MID', 'Sex', 'Phenotype'])
@@ -17,10 +19,27 @@ def write_report(output_filename, batch, bedset):
     n_has_father_in_data = has_father_in_data.shape[0]
     n_missing_fathers=n_has_father-n_has_father_in_data
     n_kinship_clusters = fam_df.drop_duplicates(subset=["FID"]).shape[0]
-    
+
     md_file.write(f"\n{n_kinship_clusters} kinship clusters")
     md_file.write(f"\n{n_has_mother} offspring with mother ID")
     md_file.write(f"\n{n_missing_mothers} mothers missing from dataset")
     md_file.write(f"\n{n_has_father} offspring with father ID")
     md_file.write(f"\n{n_missing_fathers} fathers missing from dataset")
+
+    md_file.write(f"\n## Call rates")
+    md_file.write(f"\n### Sample call rates")
+    imiss_df = pd.read_csv(imiss, delim_whitespace=True)
+    sample_call_rates = 1-imiss_df["F_MISS"]
+    md_file.write(f"\nmin: {sample_call_rates.min()}")
+    md_file.write(f"\nmax: {sample_call_rates.max()}")
+    md_file.write(f"\nmedian: {sample_call_rates.median()}")
+    outTrunk = mqc.plinkBase(output_filename)
+    plt.figure()
+    sample_call_rates.plot.hist(bins=30, alpha = 0.7, color='blue')
+    plt.title("Sample call rates")
+    plt.xlabel("Call rate")
+    plt.ylabel("Counts")
+    plt.savefig(outTrunk+'.sample_call_rates_histogram.png')
+    md_image_syntax = f'![Call Rates Histogram]({outTrunk}.call_rates_histogram.png)\n'
+    md_file.write(md_image_syntax)
     md_file.close()
