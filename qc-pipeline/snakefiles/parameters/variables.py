ef78bd244fc19dd467dfd356b3f2867d10938ea7
Øystein Kapperud <Oystein.Kapperud@fhi.no>
2024-08-07 12:20:06 +0200

    .

--- qc-pipeline/snakefiles/parameters/variables.py
+++ qc-pipeline/snakefiles/parameters/variables.py
@@ -48,6 +48,6 @@ batches = ['snp001', 'snp002', 'snp003', 'snp007', 'snp008', 'snp009', 'snp010',
 # - Batch 13 lacks documentation and needs to be checked for overlap with other batches before being added. Gutorm notes that it probably cannot be included.
 # - Batch 19 needs to be checked for overlap with other batches before being added. Gutorm notes that it probably does not need to be included.
 # batches_debug = ['snp001', 'snp002', 'snp003', 'snp007', 'snp008', 'snp009', 'snp010', 'snp011', 'snp012', 'snp014', 'snp015a', 'snp015b', 'snp016a', 'snp016b', 'snp017a', 'snp017b', 'snp017c', 'snp017d', 'snp017e', 'snp017f', 'snp018a', 'snp018b', 'snp018c', 'snp018de']
-batches_debug = ['snp003', 'snp007']
-chrs_debug = ['16', '17', '18']
+batches_debug = ['snp001', 'snp002']
+chrs_debug = ['1', '8', '9']
 # chrs_debug = [str(i) for i in range(23)]
