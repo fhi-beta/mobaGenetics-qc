5b091249827422173c2787a6b3d8d6923e5a6ab5
Øystein Kapperud <Oystein.Kapperud@fhi.no>
2024-08-29 12:48:43 +0200

    testing r2 filtering post imputation and using binary files for intermediates

--- qc-pipeline/snakefiles/parameters/variables.py
+++ qc-pipeline/snakefiles/parameters/variables.py
@@ -40,7 +40,9 @@ tmpMod6 = base/'mod6-imputation'
 tmpMergeTest = base/'merge_test'
 # use archive for these for now, since /work is full:
 tmpMod6Archive = archive_base/'mod6-imputation'
-tmpMod7 = base/'mod7-post-imputation'
+#tmpMod7 = base/'mod7-post-imputation'
+tmpMod7 = Path(config['output_base']) / '2024.08.29' / 'mod7-post-imputation'
+
 
 n_samples = config['n_samples']
 # tmpMod5 = base/'mod5-samples_unrelated'
