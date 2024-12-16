d06f6dfb2a848179f602bbfe4e6a70c032475d45
Øystein Kapperud <Oystein.Kapperud@fhi.no>
2024-12-16 15:21:38 +0100

    changed release folder

--- qc-pipeline/snakefiles/parameters/variables.py
+++ qc-pipeline/snakefiles/parameters/variables.py
@@ -45,8 +45,8 @@ tmpMod6Archive = archive_base/'mod6-imputation'
 tmpMod7 = Path(config['output_base']) / '2024.09.23' / 'mod7-post-imputation'
 # tmpMod72 = Path(config['output_base']) / '2024.09.23' / 'mod7-post-imputation'
 tmpMod8 = Path(config['output_base']) / '2024.12.03' / 'mod8-release_annotation'
-release_folder = Path(config['release_base']) / config['release']
-release_base_name = "moba_genotypes_" + config['release']
+release_folder = Path(config['release_base']) / '2024.12.03' # Path(config['release_base']) / config['release']
+release_base_name = "moba_genotypes_2024.12.03" # "moba_genotypes_" + config['release']
 
 n_samples = config['n_samples']
 # tmpMod5 = base/'mod5-samples_unrelated'
