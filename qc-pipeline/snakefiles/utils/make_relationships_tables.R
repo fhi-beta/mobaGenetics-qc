26cb1da9c901516b428d029fd91fce516c18c8ca
Øystein Kapperud <oystein.kapperud@fhi.no>
2025-11-28 04:30:02 +0100

    .

--- qc-pipeline/snakefiles/utils/make_relationships_tables.R
+++ qc-pipeline/snakefiles/utils/make_relationships_tables.R
@@ -6,7 +6,8 @@ library(dplyr)
 debug <- F
 
 if (debug){
-    args <- c("/mnt/archive2/moba_genotypes_resources/phenotypes/confirmed_relationships_25.01.30",
+    args <- c("/mnt/archive2/moba_genotypes_resources/phenotypes/expected_relationship_24.04.12.gz",
+    "/mnt/archive3/snpQc/pipeOut_dev/2025.01.30/mod8-release_annotation/mod8_pedigree_ibd_estimate.kin0",
     "/mnt/archive3/snpQc/pipeOut_dev/2025.09.25/mod8-release_annotation/mod8_batch_table_batch",
     "/mnt/work/oystein/github/mobaGenetics-qc/qc-pipeline/snakefiles/parameters/batch_chip",
     "/mnt/archive2/moba_genotypes_resources/phenotypes/ids_24.08.07.gz",
@@ -19,18 +20,24 @@ if (debug){
 }
 
 rel_file <- args[1]
-batch_file <- args[2]
-chip_file <- args[3]
-id_file <- args[4]
-relations_file <- args[5]
-shapeit_fam <- args[6]
-imputation_batches_trunk <- args[7]
-trios_file <- args[8]
+gen_file <- args[2]
+batch_file <- args[3]
+chip_file <- args[4]
+id_file <- args[5]
+relations_file <- args[6]
+shapeit_fam <- args[7]
+imputation_batches_trunk <- args[8]
+trios_file <- args[9]
 
 
 ids <- read.table(id_file, header = T) %>% select(iid = sentrix_id, reg_id = id)
 exp_rel <- read.table(rel_file, header = T, col.names=c("iid", "mat", "pat"))[,c(1,3,2)]
 exp_rel <- subset(exp_rel, !is.na(iid) & (!is.na(pat) | !is.na(mat)))
+
+gen <- read.table(gen_file, header = T)
+po <- subset(gen, InfType == "PO")
+
+
 
 # rel <- batches %>% select(iid) %>% left_join(exp_rel, by = "iid")
 
@@ -40,6 +47,16 @@ batches <- read.table(batch_file, header = T)
 batch_chip <- read.table(chip_file, header = T)
 
 rel <- batches %>% select(iid) %>% left_join(exp_rel, by = "iid")
+
+
+rel <- rel %>% left_join(po %>% select(iid = ID2, mat = ID1, mat1 = InfType), by = c("iid", "mat")) %>% left_join(po %>% select(iid = ID1, mat = ID2, mat2 = InfType), by = c("iid", "mat"))
+rel$mat_confirmed <- ifelse(!is.na(rel$mat1) | !is.na(rel$mat2), 1, 0)
+
+rel <- rel %>% left_join(po %>% select(iid = ID2, pat = ID1, pat1 = InfType), by = c("iid", "pat")) %>% left_join(po %>% select(iid = ID1, pat = ID2, pat2 = InfType), by = c("iid", "pat"))
+rel$pat_confirmed <- ifelse(!is.na(rel$pat1) | !is.na(rel$pat2), 1, 0)
+
+rel$pat <- ifelse(rel$pat_confirmed == 1, rel$pat, NA)
+rel$mat <- ifelse(rel$mat_confirmed == 1, rel$mat, NA)
 
 
 rel <- rel %>% left_join(batches %>% select(iid, iid_batch = batch), by = "iid")
@@ -65,6 +82,9 @@ rel <- rel %>% mutate(pat = ifelse(is.na(pat_batch), NA, pat)) %>% mutate(mat = 
 rel$parents <- 0
 rel$parents <- ifelse(!is.na(rel$pat), rel$parents +1, rel$parents)
 rel$parents<- ifelse(!is.na(rel$mat), rel$parents +1, rel$parents)
+
+
+
 
 rel_filtered <- rel %>%
   group_by(iid) %>%
